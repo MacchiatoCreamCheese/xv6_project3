0d3c2ef744a92332fa9618c8097621f6a04b7e3e
Nguyễn Thư Uyên <ntuyen23@apcs.fitus.edu.vn>
2026-04-18 09:52:50 +0700

    Implement thread context switching and update thread structure to include saved registers.

--- user/uthread.c
+++ user/uthread.c
@@ -11,9 +11,16 @@
 #define MAX_THREAD  4
 
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+  uint64 s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11;
+};
+
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
+  char           stack[STACK_SIZE]; /* the thread's stack */
+  int            state;             /* FREE, RUNNING, RUNNABLE */
+  struct context ctx;               /* saved callee-save registers */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -56,10 +63,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch((uint64)&t->ctx, (uint64)&next_thread->ctx);
   } else
     next_thread = 0;
 }
@@ -73,7 +77,8 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->ctx.ra = (uint64)func;
+  t->ctx.sp = (uint64)(t->stack + STACK_SIZE);
 }
 
 void 
